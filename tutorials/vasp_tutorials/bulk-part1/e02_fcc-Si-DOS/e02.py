c70c4c8dd32aa6f4258c13a40346b2e323734266
Kirill <nekirill2015@mail.ru>
2025-09-30 16:32:57 +0300

    Доделан e02 и e03

--- tutorials/vasp_tutorials/bulk-part1/e02_fcc-Si-DOS/e02.py
+++ tutorials/vasp_tutorials/bulk-part1/e02_fcc-Si-DOS/e02.py
@@ -1,18 +1,13 @@
 from pymatgen.io.vasp import Vasprun
 import matplotlib.pyplot as plt
 
-vasp = Vasprun("vasprun.xml", parse_projected_eigen=True)
+vasp = Vasprun("vasprun4.xml", parse_projected_eigen=True)
 dos = vasp.complete_dos
 
-energies = dos.energies - dos.efermi          # энергия относительно Fermi
-tdos = dos.get_densities()                    # total DOS
+energies = dos.energies - dos.efermi  # Fermi = 0
+tdos = dos.get_densities()            # total DOS
 
-plt.plot(energies, tdos, color='blue', lw=1.5)
-plt.axvline(0.0, color='k', linestyle='--', lw=0.8)  # Fermi level
-plt.xlabel("E - E_F (eV)")
-plt.ylabel("DOS (states/eV)")
-plt.title("Density of States for fcc Si")
-plt.grid(True)
-plt.savefig('e02.png')
+plt.plot(energies, tdos)
+plt.axvline(0, color='k', lw=0.8)    # Fermi
+plt.savefig('vasprun4.png')
 plt.show()
-
